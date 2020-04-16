491deef2d6b1ddb1eb8f236be7cbecafdd37284f
leluca23 <luca.ronchetti3@gmail.com>
2020-04-16 22:10:04 +0200

    2209
    2209

---
+++ Assets/Scripts/Quit_Game.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_Game : MonoBehaviour{
+    public void quitbutton()
+    {
+        Debug.Log("has quit");
+        Application.Quit();
+
+    }
+}
