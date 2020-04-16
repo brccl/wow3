7be5a04229422b2ccba333446b6fdbd86ebf032a
brccl <123ichnixdu>
2020-04-16 22:23:25 +0200

    2223
    Start_Game
    Quit_Game

--- Assets/Scripts/Switcheroo.cs
+++ Assets/Scripts/Switcheroo.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitch : MonoBehaviour
+public class Switcheroo : MonoBehaviour
 {
-    public void SceneSwitcher() {
+    public void SceneSwitcher()
+    {
         SceneManager.LoadScene(0);
 
-  }
+    }
 }
