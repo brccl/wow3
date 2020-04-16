cb3aeb8d01ea10dd379b45f7ea9d9cb9f93e749c
leluca23 <luca.ronchetti3@gmail.com>
2020-04-16 22:05:46 +0200

    controller scripts höööö

--- Assets/Scripts/ThirdPersonCameraController.cs
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCameraControler : MonoBehaviour
+public class ThirdPersonCameraController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ThirdPersonCharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float Speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
     }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizonatl");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+
+    }
+
+
 }
+
+
